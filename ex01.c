04ab349b964d7046d34345f853f36773b1593ebd
Michel Silva <104940716+DevMichelSilva@users.noreply.github.com>
2024-10-16 22:43:11 -0300

    Printing Different Variable Types
    Write a program that declares the following variables and uses printf() to print their values:
    
    - An integer (int).
    - A floating-point number (float).
    - A character (char).
    - A string (char[]).
    
    Tip: Use the format specifiers %d, %f, %c, and %s.

---
+++ ex01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+
+	int intNumber = 10;
+	float floatNumber = 3.14;
+	char caractere = 'A';
+	char string[] = "Hello world!";
+	
+	printf ("Integer Number: %d\n", intNumber);
+	printf ("Float Number: %f\n", floatNumber);
+	printf ("Caractere: %c\n", caractere);
+	printf ("String: %s\n", string);
+
+}
